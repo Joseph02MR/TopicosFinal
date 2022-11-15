89eec879c1ba00b5c49b69f5a53b929ca88f6cba
Jorge Alvarez <19030136@itcelaya.edu.mx>
2022-11-14 18:34:42 -0600

    this commit includes cart and login pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './components/pages/home';
 import Products from './components/pages/products';
 import Login from './components/pages/login';
 import NavScrollExample from './components/navbar';
+import Footer from './components/footer';
+import Cart from './components/pages/cart';
 import {
   BrowserRouter as Router,
   Routes,
@@ -17,7 +19,9 @@ function App() {
         <Route path='/' exact element={<Home/>}/>
         <Route path='/products' exact element={<Products/>}/>
         <Route path='/login' exact element={<Login/>}/>
+        <Route path='/cart' exact element={<Cart/>}/>
       </Routes>
+      <Footer/>
     </Router>
   );
   
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,14 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import UncontrolledExample from '../carrousel';
 import ProductsGroup from '../products';
-import Footer from '../footer';
-
 function home(){
     return(
         <Container fluid>
             <UncontrolledExample/>
             <ProductsGroup/>
-            <Footer/>
         </Container>
     );
 }
