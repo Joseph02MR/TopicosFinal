e841ab75c83db2b772e28dc9d9caa68f06cce0b3
Jorge Alvarez <19030136@itcelaya.edu.mx>
2022-11-17 18:41:54 -0600

    this includes

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -26,7 +26,7 @@ class login extends Component {
     e.preventDefault();
     const{email,password}=this.state;
     console.log(email,password);
-    fetch('http://localhost:8000/api/v1/login',{
+    fetch('https://joseph02mr-special-palm-tree-j9qq9gjrw7w3j5x-8000.preview.app.github.dev/api/v1/login',{
         method:"POST",
         crossDomain:true,
         headers:{
