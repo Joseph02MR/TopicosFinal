1043651b32fed9b2ced4ce4f89ed2391d578450d
jorgeSilis <84548055+jorgeSilis@users.noreply.github.com>
2022-11-22 19:14:43 -0600

    Paginacion terminada
    paginacion implementada para que no se pierda el mnmñumñieco

---
+++ src/components/pages/profile.js
@@ -0,0 +1,167 @@
+import logo from "../../assets/logo_store.png";
+import React, { Component } from "react";
+import {
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBCard,
+    MDBCardText,
+    MDBCardBody,
+    MDBCardImage,
+    MDBBtn,
+    MDBBreadcrumb,
+    MDBBreadcrumbItem
+} from "mdb-react-ui-kit";
+import { NavLink } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+ function profile ()  {
+    const profile = JSON.parse(localStorage.getItem('SESSION'));
+    console.log(profile);
+
+  
+    return (
+      <Container fluid style={{ margin: "auto" }}>
+        <Row style={{ alignContent: "center", margin: 30 }}>
+          <Col md={6} style={{ textAlign: "center", margin: "auto" }}>
+            <div>
+              <div className="text-center">
+                <img src={logo} style={{ width: "185px" }} alt="logo" />
+                <h4 className="mt-1 mb-5 pb-1">We are Le José Team</h4>
+              </div>
+            </div>
+          </Col>
+
+
+          <section style={{ backgroundColor: '#eee' }}>
+      <MDBContainer className="py-5">
+        <MDBRow>
+          <MDBCol>
+            <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
+              <MDBBreadcrumbItem active>User Profile</MDBBreadcrumbItem>
+            </MDBBreadcrumb>
+          </MDBCol>
+        </MDBRow>
+
+        <MDBRow>
+          <MDBCol lg="4">
+            <MDBCard className="mb-4">
+              <MDBCardBody className="text-center">
+                <MDBCardImage
+                  src={profile["image"]}
+                  alt="avatar"
+                  className="rounded-circle"
+                  style={{ width: '150px' }}
+                  fluid />
+                <br>
+                </br>
+                <br>
+                </br>
+                <div className="d-flex justify-content-center mb-2">
+                  <MDBBtn>Editar</MDBBtn>
+                  <MDBBtn outline className="ms-1">Eliminar</MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+
+          <MDBCol lg="8">
+            <MDBCard className="mb-4">
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Full Name</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{profile["name"]}  {profile["lastname"]}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Email</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{profile["email"]}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Phone</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{profile["phone"]}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Country</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Mexico</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>State</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Guanajuato</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>City</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Celaya</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Street</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Bay Area, San Francisco, CA</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Gender</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{profile["gender"]}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Username</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{profile["username"]}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+
+            
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
+          
+        </Row>
+      </Container>
+    );
+  }
+
+export default profile;
--- src/components/products.js
+++ src/components/products.js
@@ -2,17 +2,29 @@ import { Button, Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import React, {useContext} from 'react';
+import React, {useContext, useRef, useState} from 'react';
 import { Datacontext } from '../context/DataProvider';
 
+const prods_pp = 20; //productos por pagina
+
 function ProductsGroup() {
+  const quantityPageRef = useRef(12);
+  const [currentIndex,setCurrentIndex] = useState(0);
+  const [endPosition, setEndPosition] = useState(12);
   const value = useContext(Datacontext);
   const[productos] = value.productos;
   //console.log(productos);
+
+  const handlePagination = (index) => {
+
+    setEndPosition( (index + 1 ) * quantityPageRef.current);
+    setCurrentIndex( (index + 1 ) * quantityPageRef.current - quantityPageRef.current );
+
+  }
     return (
       <Container fluid>
         <Row xs={1} sm={2} md={3} lg={4} className="g-2">
-        {productos.map((product)=>(
+        {productos.slice(currentIndex,endPosition).map((product)=>(
             <Col style={{ padding:15 }}>
               <Card class="card" style={{ objectFit:'contain', Height:350 }} >
                 <><Card.Img style={{ objectFit:'contain', height:250 }} variant="top" class="cards-img" src={product.images[0]} /><Card.Body>
@@ -26,6 +38,10 @@ function ProductsGroup() {
             </Col>
             ))}
         </Row>
+        {Array(Math.ceil(productos.length / quantityPageRef.current)).fill(null).map((_,index) =>(
+
+          <Button  variant="outline-primary" style={{ margin:2, marginLeft:10}} className={`${currentIndex === 0 && index === currentIndex ? 'active' : index === currentIndex/quantityPageRef.current && "active"  }`} onClick={()=>handlePagination(index)}>{index + 1}</Button>
+        ))}
         </Container>
       );
 }
